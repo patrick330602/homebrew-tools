30d2743c660bc35d18f3c647febb8e66e9667321
Patrick Wu <me@patrickwu.space>
2022-07-27 14:08:17 +0800

    add tltk 0.9

--- Formula/tltk.rb
+++ Formula/tltk.rb
@@ -1,20 +1,14 @@
 class Tltk < Formula
   desc "Patrick's Tiny Little Toolkit"
   homepage "https://github.com/patrick330602/tltk"
-  url "https://github.com/patrick330602/tltk/archive/v0.8.tar.gz"
-  sha256 "6a96173373107c53c4963c452eec8a8336fe6aab714af5db6f071a4364a9d074"
+  url "https://github.com/patrick330602/tltk/archive/v0.9.tar.gz"
+  sha256 "be488e6db9c8cf1959195ee38d8dc9caed77c2403402e84a1dcc37fc15c36cc0"
   head "https://github.com/patrick330602/tltk.git"
 
   depends_on "python"
 
   def install
     system "mkdir", "-p", "#{prefix}/bin"
-    system "cp", "-p", "src/colortest", "#{prefix}/bin/colortest"
-    system "cp", "-p", "src/iconfinder", "#{prefix}/bin/iconfinder"
-    system "cp", "-p", "src/paheal", "#{prefix}/bin/paheal"
-    system "cp", "-p", "src/pswd-gen", "#{prefix}/bin/pswd-gen"
-    system "cp", "-p", "src/tumblr", "#{prefix}/bin/tumblr"
-    system "cp", "-p", "src/unzip-jp", "#{prefix}/bin/unzip-jp"
-    system "cp", "-p", "src/get-ba-ost", "#{prefix}/bin/get-ba-ost"
+    system "cp", "-p", "src/*", "#{prefix}/bin/"
   end
 end
